b23652fa220d39f6feb8db2d051c8a89c99bf32b
vvduth <ducthai060501@gmail.com>
2021-07-21 02:42:03 +0300

    "Contact List items"

--- app/src/main/java/org/mycode/android_part_3/ContactRecViewAdapter.java
+++ app/src/main/java/org/mycode/android_part_3/ContactRecViewAdapter.java
@@ -1,8 +1,12 @@
 package org.mycode.android_part_3;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,22 +16,40 @@ import java.util.ArrayList;
 public class ContactRecViewAdapter extends RecyclerView.Adapter<ContactRecViewAdapter.ViewHolder> {
 
     private ArrayList<Contact> contacts = new ArrayList<>();
+    private Context context ;
 
-
-    public ContactRecViewAdapter(){
-
+    public ContactRecViewAdapter(Context context){
+        this.context = context ;
     }
 
     @NonNull
     @org.jetbrains.annotations.NotNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contacts_list_item, parent, false); //inflate a layout
+        // Inside the main, we can use getLayoutInflator but in a class, we can not, we use from and inflate instead
+        // The View group is the parent of all the layouts, the view group can be used to group different views inside this
+        // if the boolean is true, it will be redundant
+        //if we not sure about the parent, or about where our view is going to be attached, use null
+
+        ViewHolder holder = new ViewHolder(view);
+        return holder ;
+        // this holder after return will be pass into onBindViewHolder method
+        // display all elements of the view holder class to recycle view
     }
 
     @Override
     public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull ContactRecViewAdapter.ViewHolder holder, int position) {
-
+        //this methods in the parents clas can access to al private attributes of the child class
+        //position get from recViewadapter
+        // the most important method, from this can set onlickListener to every UI elements
+        holder.txtName.setText(contacts.get(position).getName());
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, contacts.get(position).getName() + " Selected",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -40,14 +62,17 @@ public class ContactRecViewAdapter extends RecyclerView.Adapter<ContactRecViewAd
         notifyDataSetChanged(); //notify the adapter that the data has been changed, refer the recycle view with the new data(the data that pass to this class)
     }
 
-    private TextView txtName ;
+
 
     //hold the view for every item in recycle view, generating view objects
     public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView txtName ;
+        private RelativeLayout parent ;
         public ViewHolder(@NonNull  View itemView) {
             super(itemView);
             //because this is not main activity, we need to put itemView
             txtName = itemView.findViewById(R.id.txtName);
+            parent = itemView.findViewById(R.id.parent);
         }
     }
 }
--- app/src/main/java/org/mycode/android_part_3/MainActivity.java
+++ app/src/main/java/org/mycode/android_part_3/MainActivity.java
@@ -1,19 +1,41 @@
 package org.mycode.android_part_3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private RecyclerView  contactsRecView ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         contactsRecView = findViewById(R.id.contactsRecView);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+        contacts.add(new Contact("Duc Thai", "ducthai@gmail.com","https://vvduth.github.io/TAHwebsite/images/works/thaiducweb.jpg"));
+        contacts.add(new Contact("Anh Tai", "anhtai@gmail.com","https://vvduth.github.io/TAHwebsite/images/works/anhtaiweb.png"));
+        contacts.add(new Contact("Mai Manh", "maimanh@gmail.com","https://vvduth.github.io/TAHwebsite/images/works/maimanhweb.jpg"));
+        contacts.add(new Contact("Que Hung", "quehung@gmail.com","https://vvduth.github.io/TAHwebsite/images/works/quehungweb.jpg"));
+        contacts.add(new Contact("Dinh Duy", "duysop@gmail.com","https://vvduth.github.io/TAHwebsite/images/works/dinhduyweb.jpg"));
+        contacts.add(new Contact("Huu Tai", "taibeo@gmail.com","https://vvduth.github.io/TAHwebsite/images/works/huutaiweb.png"));
+
+        ContactRecViewAdapter adapter = new ContactRecViewAdapter(this);
+        adapter.setContacts(contacts);
+
+        contactsRecView.setAdapter(adapter);
+        //contactsRecView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false));  //display our items on a linear fashion
+        contactsRecView.setLayoutManager(new GridLayoutManager(this, 2 ));
+
     }
 }
